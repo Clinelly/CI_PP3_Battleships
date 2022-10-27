86603f02a54a85a9b28447f0196367f1e87237c4
Clinelly <swall289@gmail.com>
2022-10-27 20:00:52 +0000

    Add basic function layout to run.py

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def main_screen():
+    """
+    A function to generate the main screen before the game starts.
+    ASCII art dashboard and asks player to start game.
+    Asks player for a name + difficulty.
+    """
+
+def run_game():
+    """
+    Main function. Will incorporate board and ship generation.
+    """
+
+def generate_board():
+    """
+    Will generate the game board. Either a straight figure print
+    or generate from lists/dictionary.
+    Difficulties will change board size.
+    """
+
+def generate_ships():
+    """
+    Will generate a series of ships.
+    Ship sizes preset but locations generated randomly.
+    """
+
+def ship_shooting():
+    """
+    Takes the user input and checks for validation.
+    Assigns the shot to a co-ordinate and checks for hit/miss/sink.
+    Generates computer shot.
+    Feeds back to user.
+    """
+
+def game_over():
+    """
+    Runs when all ships sunk.
+    Prompts user to restart or exit.
+    """
